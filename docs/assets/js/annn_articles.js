76aa6f2bc8e461290ff59d33b0f320e83f3806c8
unknown <mqmqspark@gmail.com>
2020-08-21 01:15:42 +0900

    20200820 yoru update html js css

--- docs/assets/js/annn_articles.js
+++ docs/assets/js/annn_articles.js
@@ -3,6 +3,10 @@ document.onmousedown='';
 document.body.onselectstart='';
 document.body.onmousedown='';
 
+/*
+関数群
+*/
+
 function showRika() {
   var aa_obj = document.getElementById("AA_txt")
 
@@ -37,3 +41,40 @@ function delAA() {
   aa_obj.innerText = "";
   aa_obj.removeAttribute("style");
 };
+
+
+/*
+スマートフォン判定
+max-device-width: 640px
+*/
+function isSmartPhone() {
+  if (window.matchMedia && window.matchMedia("(max-device-width: 1280px)").matches) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/*PCスタイル*/
+function setPCStyle() {
+  var article_body_obj = document.getElementById("article_body");
+  article_body_obj.style.width = "1000px"
+}
+
+/*スマートフォンスタイル*/
+function setSmartPhoneStyle() {
+  var article_body_obj = document.getElementById("article_body");
+  article_body_obj.style.fontSize = "16pt"
+  article_body_obj.style.width = "40em"
+}
+
+
+/*
+メイン実行
+*/
+
+/* スマートフォンの場合スマートフォンのstyle設定を行う */
+if (isSmartPhone()) {
+  console.log("set smartphone style")
+  setSmartPhoneStyle()
+}
